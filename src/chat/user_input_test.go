a99a95ae6da10d35704ad94628e21b32f40377d0
spring1843 <nicolas@Nicolass-iMac.local>
2017-02-01 21:24:09 -0800

    Check that the user can keep sending messages after a failure

--- src/chat/user_input_test.go
+++ src/chat/user_input_test.go
@@ -22,7 +22,38 @@ func TestHandleCommandInput(t *testing.T) {
 
 	incoming := user1.GetOutgoing()
 	if !strings.Contains(incoming, "quit") {
+		t.Errorf("Message was not sent to the user, expected quit to be part of %s", incoming)
+	}
+}
+
+func TestHandleCommandInputFailure(t *testing.T) {
+	server := chat.NewServer()
+	user1 := chat.NewUser("u1")
+
+	server.AddUser(user1)
+
+	go func(t *testing.T) {
+		input := `/yelp`
+		if _, err := user1.HandleNewInput(server, input); err == nil {
+			t.Fatal("Did not fail executing invalid command", err)
+		}
+	}(t)
+
+	incoming := user1.GetOutgoing()
+	if !strings.Contains(incoming, "not found") {
 		t.Errorf("Message was not read from the user, expected quit to be part of %s", incoming)
+	}
+
+	go func(t *testing.T) {
+		input := `/help`
+		if _, err := user1.HandleNewInput(server, input); err != nil {
+			t.Fatalf("Failed executing help command. Error %s", err)
+		}
+	}(t)
+
+	incoming = user1.GetOutgoing()
+	if !strings.Contains(incoming, "quit") {
+		t.Errorf("Message was not sent to the user, expected quit to be part of %s", incoming)
 	}
 }
 
