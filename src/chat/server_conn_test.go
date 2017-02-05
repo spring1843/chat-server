80cd251fbeeef8a4efd5de39074f294982379b5d
nicolas <nicolas@Nicolass-iMac.local>
2017-02-04 18:50:04 -0800

    change test to try multiple users

--- src/chat/server_conn_test.go
+++ src/chat/server_conn_test.go
@@ -1,31 +1,35 @@
 package chat_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/spring1843/chat-server/src/chat"
 	"github.com/spring1843/chat-server/src/drivers/fake"
 )
 
-func TestInterviewUser(t *testing.T) {
-	var (
-		server     = chat.NewServer()
-		connection = fake.NewFakeConnection()
-	)
-
+func TestUserInterview(t *testing.T) {
+	tryouts := 10
+	server := chat.NewServer()
 	server.Listen()
 
-	input := "newuser\n"
-	n, err := connection.WriteString(input)
-	if err != nil {
-		t.Fatalf("Failed writing to connection. Error %s", err)
-	}
-	if n != len(input) {
-		t.Fatalf("Wrong length after write. Expected %d, got %d.", len(input), n)
-	}
+	connections := make([]*fake.MockedConnection, tryouts, tryouts)
+	i := 0
+	for i < tryouts {
+		connections[i] = fake.NewFakeConnection()
 
-	server.InterviewUser(connection)
-	if server.ConnectedUsersCount() != 1 {
-		t.Errorf("User was not added to the server")
+		nickName := fmt.Sprintf("user%d", i)
+		n, err := connections[i].WriteString(nickName)
+		if err != nil {
+			t.Fatalf("Failed writing to connection for user %q. Error %s", nickName, err)
+		}
+		if n != len(nickName) {
+			t.Fatalf("Wrong length after write. For user %s Expected %d, got %d.", nickName, len(nickName), n)
+		}
+		server.InterviewUser(connections[i])
+		if server.ConnectedUsersCount() != i+1 {
+			t.Errorf("User %s was not added to the server", nickName)
+		}
+		i++
 	}
 }
