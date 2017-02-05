c2a80866e8a0f82254a498f1bb3e70cd3a336841
nicolas <nicolas@Nicolass-iMac.local>
2017-02-05 13:52:08 -0800

    If config address is empty for telnet or web, do not serve

--- src/bootstrap.go
+++ src/bootstrap.go
@@ -18,8 +18,30 @@ func bootstrap(config config.Config) {
 	chatServer.Listen()
 	logs.Info("Chat Server started")
 
-	logs.FatalIfErrf(startTelnet(config, chatServer), "Could not start telnet server.")
+	if config.TelnetAddress != "" {
+		logs.FatalIfErrf(startTelnet(config, chatServer), "Could not start telnet server.")
+	} else {
+		logs.Warnf("TelnetAddress is empty, not running Telnet Driver")
+	}
 
+	if config.WebAddress != "" {
+		logs.FatalIfErrf(startWeb(config, chatServer), "Could not start web server.")
+	} else {
+		logs.Warnf("WebAddress is empty, not running Web Drivers")
+	}
+}
+
+func startTelnet(config config.Config, chatServer *chat.Server) error {
+	err := telnet.Start(chatServer, config)
+	if err != nil {
+		return err
+
+	}
+	logs.Info("Telnet server started")
+	return nil
+}
+
+func startWeb(config config.Config, chatServer *chat.Server) error {
 	restHandler := rest.GetHandler(chatServer)
 	websocket.SetWebSocket(chatServer)
 	fs := http.FileServer(http.Dir(staticWebContentDir))
@@ -32,14 +54,5 @@ func bootstrap(config config.Config) {
 		logs.Infof("Serving static files, Rest, WebSocket on http:/%s/", config.WebAddress)
 		logs.FatalIfErrf(http.ListenAndServeTLS(config.WebAddress, "tls.crt", "tls.key", nil), "Could not start Rest server.")
 	}()
-}
-
-func startTelnet(config config.Config, chatServer *chat.Server) error {
-	err := telnet.Start(chatServer, config)
-	if err != nil {
-		return err
-
-	}
-	logs.Info("Telnet server started")
 	return nil
 }
